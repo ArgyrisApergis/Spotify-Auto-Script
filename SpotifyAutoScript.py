3462777ca4c9c42b47b1fe09bcb288ccef55faaf
ArgyrisApergis <argyris.apergis@gmail.com>
2023-11-13 21:32:09 +0200

    final v

---
+++ SpotifyAutoScript.py
@@ -0,0 +1,37 @@
+import time
+import pyautogui
+import webbrowser
+
+#--- For opening the url ---
+  
+# url = "https://open.spotify.com/search" # Replace with the URL of the webpage you want to open
+# # Open the webpage in the default web browser
+# webbrowser.open(url)
+# time.sleep(8) # Give it some time to load the page
+
+# pyautogui.write(" LEX mousiki gia", interval=0.5) # add the name of the song
+# time.sleep(2)
+# pyautogui.click( x = 973, y = 558, button='left', clicks=1)
+
+# time.sleep(2)
+
+# # The following lines help determine the possistion of the cursor to use it with pyautogui
+# current_x, current_y = pyautogui.position()
+# print(f"Current Mouse Position: x={current_x}, y={current_y}")
+
+
+#--- For opening the application ---
+
+# import subprocess
+# # Specify the path to the Spotify executable on your system
+# spotify_executable = "C:/Users/Archie/AppData/Roaming/Spotify/Spotify.exe" # Update this with the actual path to spotify.exe
+
+# # Open Spotify
+# subprocess.Popen([spotify_executable])
+# time.sleep(5) # Give it some time to load the app
+
+# pyautogui.hotkey("ctrl", "l")
+# time.sleep(2)
+# pyautogui.write(" LEX mousiki gia", interval=0.5) # Update this with the name of the song
+
+# pyautogui.click( x = 555, y = 481, button='left', clicks=1)
